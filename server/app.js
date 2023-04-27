7a1127c11daaa9bddea1f389559674de25dcb096
Sara Hamza <sarasaadoun4@gmail.com>
2023-04-27 08:32:45 +0200

    added in userSchema, edited usrValidationErrHandler

--- server/app.js
+++ server/app.js
@@ -3,20 +3,29 @@ const mongoose = require('mongoose')
 const authRoutes = require('./routes/authRoutes')
 //TODO: cookieParser pkg
 
+require('dotenv').config()
+
 const app = express()
 
 // middleware
 
 app.use(express.json()) // parse incoming requests with JSON payloads
 
+const port = process.env.PORT || 3000
 // Connecting to db
-const dbURI =
-  'mongodb+srv://test-user:test1234@cluster0.iqkprwq.mongodb.net/shopliy'
+const dbURI = process.env.DB_URI
 
 mongoose
   .connect(dbURI)
-  .then((result) => app.listen(3000))
-  .catch((err) => console.log(`db connection error: ${err}`))
+  .then(() => {
+    console.log('Connected to MongoDB!')
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`)
+    })
+  })
+  .catch((err) => {
+    console.error(`Error connecting to MongoDB: ${err}`)
+  })
 
 app.get('/', (req, res) => {
   res.json('Home')
