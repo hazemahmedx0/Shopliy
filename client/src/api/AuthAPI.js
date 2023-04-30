76ef2f531aa2fc323eb34d6b16f749225cdbe45c
Hazem <hazem31009444@f-eng.tanta.edu.eg>
2023-04-30 03:21:19 +0300

    Signup api

--- client/src/api/AuthAPI.js
+++ client/src/api/AuthAPI.js
@@ -1,7 +1,7 @@
 import axiosClient from './axiosClient'
 
 const authAPI = {
-  // signup: (params) => axiosClient.post('users/signup', params),
+  signup: (params) => axiosClient.post('signup', params),
   // login: (params) => axiosClient.post('users/login', params),
   verifyUser: () => axiosClient.get('me'),
 }
