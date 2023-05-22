e4d58b51803dacac79ad438360d677f6368ad310
Hazem <hazem31009444@f-eng.tanta.edu.eg>
2023-05-22 09:15:11 +0300

    gg

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import queryString from 'query-string'
 
-const baseUrl = 'https://shopliy-production-ee12.up.railway.app/'
+const baseUrl = 'https://api.hazemmahdyd.net/availableProducts/'
 
 const axiosClient = axios.create({
   baseURL: baseUrl,
--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: 'https://shopliy-test.vercel.app',
+    origin: true,
     credentials: true,
   })
 )
