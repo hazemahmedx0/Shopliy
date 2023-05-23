fcd94ad5e05c04ea14dc4a252a50730e0e07a7f4
Sara Hamza <sarasaadoun4@gmail.com>
2023-05-23 21:36:51 +0200

    add delete to wishlist

--- server/controllers/wishlistControllers.js
+++ server/controllers/wishlistControllers.js
@@ -1,45 +1,57 @@
 const {
-    getWishlist,
-    getWishlistWithProductDetails,
-    addNewItem,
-
-  } = require('../utils/wishlistUtils')
-
-
+  getWishlist,
+  getWishlistWithProductDetails,
+  addWishlistItem,
+  deleteWishlistItem,
+} = require('../utils/wishlistUtils')
 
 const getMyWishlist = async (req, res) => {
-    const userId = res.locals.user._id
-    try {
-      let wishlist = await getWishlist(userId)
-      wishlist = await getWishlistWithProductDetails(wishlist._id)
-      
-      res.json(wishlist)
-    } catch (err) {
-      console.log(err)
-      return res.status(500).json({ message: 'Internal server error' })
-    }
-  }
+  const userId = res.locals.user._id
+  try {
+    let wishlist = await getWishlist(userId)
+    wishlist = await getWishlistWithProductDetails(wishlist._id)
 
-  const addProductToWishlist = async (req, res) => {
-    const productId = req.params.productId
-    const userId = res.locals.user._id
-    console.log(productId, userId)
-    try {
-      const item = {productId}
-      let wishlist = await getWishlist(userId)
-      wishlist = await addNewItem(wishlist, item)
-      wishlist = await getWishlistWithProductDetails(wishlist._id)
-      console.log(wishlist.items)
-  
-      res.json({ message: 'added Item successfully', wishlist })
-    } catch (err) {
-      console.log(err)
-      return res.status(500).json({ message: 'Internal server error' })
-    }
+    res.json(wishlist)
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({ message: 'Internal server error' })
   }
+}
 
-  module.exports = {
-    getMyWishlist,
-    addProductToWishlist,
+const addProductToWishlist = async (req, res) => {
+  const productId = req.params.productId
+  const userId = res.locals.user._id
+  try {
+    const item = { productId }
+    let wishlist = await getWishlist(userId)
+    wishlist = await addWishlistItem(wishlist, item)
+    wishlist = await getWishlistWithProductDetails(wishlist._id)
+    console.log(wishlist.items)
+
+    res.json({ message: 'added Item successfully', wishlist })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({ message: 'Internal server error' })
   }
-  
+}
+
+const deleteProductFromWishlist = async (req, res) => {
+  const productId = req.params.productId
+  const userId = res.locals.user._id
+  try {
+    let wishlist = await getWishlist(userId)
+    wishlist = await deleteWishlistItem(wishlist, productId)
+    wishlist = await getWishlistWithProductDetails(wishlist._id)
+
+    res.json({ message: 'deleted Item successfully', wishlist })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({ message: 'Internal server error' })
+  }
+}
+
+module.exports = {
+  getMyWishlist,
+  addProductToWishlist,
+  deleteProductFromWishlist,
+}
