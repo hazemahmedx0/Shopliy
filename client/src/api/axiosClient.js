c6058ac96a9e3c75a28ea962e024676daf50be04
Hazem Mahdy <hazem31009444@f-eng.tanta.edu.eg>
2024-05-16 04:25:48 +0400

    Update axiosClient.js

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -5,7 +5,7 @@ axios.defaults.withCredentials = true
 // todo: Token from Cookies
 
 // const baseUrl = 'https://api.hazemmahdyd.net/'
-const baseUrl = 'http://localhost:3000/'
+const baseUrl = 'https://shopliy.onrender.com/'
 
 const axiosClient = axios.create({
   baseURL: baseUrl,
